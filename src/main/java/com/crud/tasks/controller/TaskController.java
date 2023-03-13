34b23d8ba23bc5afe2f50fde0e6f6bb9e79c1fd7
Rafal <rafsmo1992@gmail.com>
2023-03-13 11:10:11 +0100

    ID working as expected

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,12 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,15 +14,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
     @GetMapping
     public List<TaskDto> getTasks() {
-     return new ArrayList<>();
+    List<Task>tasks = service.getAllTasks();
+    return taskMapper.mapToTaskDtoList(tasks);
     }
     @GetMapping(value = "{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId)
-    {
-        return new TaskDto(1L,"test title", "test_content");
+    public TaskDto getTask(@PathVariable Long taskId) throws TaskNotFoundException
+    { return taskMapper.mapToTaskDto(
+            service.getTask(taskId).orElseThrow(TaskNotFoundException::new)
+    );
     }
     @DeleteMapping
     public void deleteTask(long taskId)
@@ -27,9 +40,10 @@ public class TaskController {
     {
     return new TaskDto(1L,"Edited test title", "Test content");
     }
-    @PostMapping
-    public void createTask(TaskDto taskDto)
-    {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
     }
     }
 
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,27 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import lombok.*;
+import org.springframework.stereotype.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DbService {
+
+    private final TaskRepository repository;
+
+    public List<Task> getAllTasks(){
+        return repository.findAll();
+    }
+    public Optional<Task> getTask(final Long id)
+    {
+        return repository.findById(id);
+    }
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+}
