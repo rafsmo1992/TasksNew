b2aca34d8a6bdb0fb495ed3f32eb879def81f675
Rafal <rafsmo1992@gmail.com>
2023-03-13 06:54:21 +0100

    first commit

---
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -0,0 +1,12 @@
+package com.crud.tasks;
+
+import com.crud.tasks.domain.TaskDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+}
